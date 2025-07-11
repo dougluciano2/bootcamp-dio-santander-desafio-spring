96c34ad7c77b5f05b58243f7d265bc8752bb0e2e
Douglas O. Luciano <douglas.luciano2@gmail.com>
2025-07-10 21:09:32 -0300

    fix/remove flyway references

--- src/main/java/br/com/dougluciano/dio/santander/bootcamp/desafiospring/Application.java
+++ src/main/java/br/com/dougluciano/dio/santander/bootcamp/desafiospring/Application.java
@@ -1,18 +1,7 @@
 package br.com.dougluciano.dio.santander.bootcamp.desafiospring;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.context.annotation.Bean;
-
-import jakarta.persistence.EntityManagerFactory;
-import org.springframework.boot.autoconfigure.orm.jpa.JpaProperties;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
-import org.springframework.context.annotation.DependsOn;
-import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
-
-import javax.sql.DataSource;
 
 @SpringBootApplication
 public class Application {
--- src/main/java/br/com/dougluciano/dio/santander/bootcamp/desafiospring/config/SecurityConfig.java
+++ src/main/java/br/com/dougluciano/dio/santander/bootcamp/desafiospring/config/SecurityConfig.java
@@ -1,24 +1,18 @@
 package br.com.dougluciano.dio.santander.bootcamp.desafiospring.config;
 
 import br.com.dougluciano.dio.santander.bootcamp.desafiospring.filter.SecurityFilter;
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
-import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -34,7 +28,7 @@ public class SecurityConfig {
     @Autowired
     private SecurityFilter securityFilter;
 
-    public SecurityConfig(SecurityFilter securityFilter, Flyway flyway) {
+    public SecurityConfig(SecurityFilter securityFilter) {
         this.securityFilter = securityFilter;
     }
 
